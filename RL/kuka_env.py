9f7b2ba4042e9bf53d814f0584258f2e34eda580
ekin <6nekincanpolat@gmailcom>
2025-02-11 17:12:41 +0100

    Updated

---
+++ RL/kuka_env.py
@@ -0,0 +1,92 @@
+import gymnasium as gym
+import pybullet as p
+import pybullet_data
+import numpy as np
+from gymnasium import spaces
+
+
+class KukaEnv(gym.Env):
+    def __init__(self):
+        super(KukaEnv, self).__init__()
+
+        # PyBullet connection
+        self.physics_client = p.connect(p.GUI)
+        p.setAdditionalSearchPath(pybullet_data.getDataPath())
+
+        # for kuka load
+        self.reset()
+
+        #Action space: (Continuous)**: For 7 joints between [-1, 1] continuous actions
+        #tanh(x) inputs for PPO agent activation and it requires [-1,1]-> helps more stabilizated learning
+        #the joint limits are taken as [-90,90] degrees, so optimization in this gap wpuld be more covenient
+        self.action_space = spaces.Box(low=-1, high=1, shape=(7,), dtype=np.float32)
+
+        # Observation Space**: Robot joint+ end efector positions
+        # 7 degrees of freedom and 3 for end efector position(x-y-z orientation)
+        self.observation_space = spaces.Box(
+            low=-1, high=1, shape=(10,), dtype=np.float32
+        )
+
+
+#for each training period, in the start to take the agent to initial and reset the environment
+    def reset(self, seed=None, options=None):
+        super().reset(seed=seed)
+
+        p.resetSimulation(self.physics_client)
+        p.setGravity(0, 0, -9.8)
+        p.setAdditionalSearchPath(pybullet_data.getDataPath())
+
+        self.plane_id = p.loadURDF("plane.urdf")
+        self.robot_id = p.loadURDF("kuka_iiwa/model.urdf", useFixedBase=True)
+
+        # taking robot to the initial
+
+        self.initial_joint_positions = [0] * 7
+        for i in range(7):
+            p.resetJointState(self.robot_id, i, self.initial_joint_positions[i])
+
+        return self._get_observation()
+
+
+#to make the actions work on robot
+    def step(self, action):
+
+        for i in range(7):
+            p.setJointMotorControl2(self.robot_id, i, p.POSITION_CONTROL, action[i])
+
+        # continues withb the next step of the simulation
+        p.stepSimulation()
+
+        # get new statues
+        obs = self._get_observation()
+
+        ############ REWARD FUNCTION ############
+
+        #job: End efector gets close to a target
+        end_effector_pos = self._get_end_effector_position()
+        target_pos = [0.5, 0.5, 0.5]
+        distance_to_target = np.linalg.norm(np.array(end_effector_pos) - np.array(target_pos))
+
+        ############ REWARD CALCULATIONS ############
+        reward = -distance_to_target
+
+        # if its's so close to the target, reward higher the robot
+        done = distance_to_target < 0.05
+        if done:
+            reward += 10
+
+        info = {}
+
+#downwards two func gets the current situations
+    def _get_observation(self):
+        joint_states = [p.getJointState(self.robot_id, i)[0] for i in range(7)]
+        end_effector_pos = self._get_end_effector_position()
+        return np.array(joint_states + list(end_effector_pos), dtype=np.float32)
+
+    def _get_end_effector_position(self):
+        end_effector_index = 6
+        link_state = p.getLinkState(self.robot_id, end_effector_index)
+        return link_state[0]
+
+    def close(self):
+        p.disconnect(self.physics_client)
---
+++ RL/train_ppo.py
@@ -0,0 +1,15 @@
+from stable_baselines3 import PPO
+from stable_baselines3.common.vec_env import DummyVecEnv
+from kuka_env import KukaEnv
+
+env = DummyVecEnv([lambda: KukaEnv()])
+# PPO model setup
+#Policy of Neural N:"MLP" (Multi-Layer Perceptron)
+#Verbose-> prints out the state
+model = PPO("MlpPolicy", env, verbose=1)
+
+model.learn(total_timesteps=100000)
+
+model.save("ppo_kuka_iiwa")
+
+print("Training finished! Model saved.")
